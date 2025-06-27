4cdca8593a7ea85ba6b7faac4ca72bcbc0a172b5
Tanmayraut16 <Tanmayraut16@outlook.com>
2025-06-27 16:27:39 +0530

    Initial commit

---
+++ styles/auth.styles.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native"
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    color: "#000",
+    fontSize: 20,
+    fontWeight: "bold",
+  }
+})
